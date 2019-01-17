01b7534c97323b46c6b622f7c93dad2aec77716e
Dimitar Mihaylov <dimitar.mihaylove@gmail.com>
2019-01-17 20:35:41 +0200

    step1

--- Project/Interpretator/Interpretator.cpp
+++ Project/Interpretator/Interpretator.cpp
@@ -8,9 +8,11 @@
 #include <stdlib.h>
 #include <locale>
 
-#define INTEGER ("INTEGER")
-#define PLUS ("PLUS")
-#define EOF ("EOF")
+std::string INTEGER = "INTEGER";
+std::string PLUS = "PLUS";
+std::string MINUS = "MINUS";
+char NULL_CHAR = '\0';
+std::string EoF = "EOF";
 std::string::size_type sz;
 
 struct Token
@@ -41,25 +43,38 @@ struct Interpreter
 {
 	std::string text;
 	int pos = 0;
-	std::string currentToken = nullptr;
-	char* currentChar = &text[this->pos];
+	Token* currentToken = nullptr;
+	char currentChar;
+
+	Interpreter(std::string _text)
+	{
+		this->text = _text;
+		this->pos = 0;
+		this->currentToken = nullptr;
+		this->currentChar = this->text[this->pos];
+	}
+
+	void Error()
+	{
+		throw std::exception("Error");
+	}
 
 	void Advance()
 	{
 		this->pos++;
 		if (this->pos > this->text.size() - 1)
 		{
-			this->currentChar = nullptr;
+			this->currentChar = NULL_CHAR;
 		}
 		else
 		{
-			this->currentChar = &this->text[this->pos];
+			this->currentChar = this->text[this->pos];
 		}
 	}
 
 	void skipWhitespace()
 	{
-		while (this->currentChar != nullptr &&  this->currentChar != " ")
+		while (this->currentChar != NULL_CHAR &&  this->currentChar == ' ')
 		{
 			this->Advance();
 		}
@@ -68,7 +83,7 @@ struct Interpreter
 	int Integer()
 	{
 		std::string result = "";
-		while (this->currentChar != nullptr && isdigit(std::stoi(this->currentChar)))
+		while (this->currentChar != NULL_CHAR && isdigit(this->currentChar))
 		{
 			result += this->currentChar;
 			this->Advance();
@@ -77,27 +92,98 @@ struct Interpreter
 		return std::stoi(result);
 	}
 
+	Token* GetNextToken()
+	{
+		while (this->currentChar != NULL_CHAR)
+		{
+			if (this->currentChar == ' ')
+			{
+				this->skipWhitespace();
+				continue;
+			}
+			
+			if (isdigit(this->currentChar))
+			{
+				return new Token(INTEGER, std::to_string(this->Integer()));
+			}
+
+			if (this->currentChar == '+')
+			{
+				this->Advance();
+				return new Token(PLUS, "+");
+			}
+
+			if (this->currentChar == '-')
+			{
+				this->Advance();
+				return new Token(MINUS, "-");
+			}
+
+			this->Error();
+		}
+
+		return new Token(EoF, EoF);
+	}
+
+	void Eat(std::string tokenType)
+	{
+		if (this->currentToken->type == tokenType)
+		{
+			this->currentToken = this->GetNextToken();
+		}
+		else
+		{
+			this->Error();
+		}
+	}
+
+	unsigned long int Expression()
+	{
+		this->currentToken = this->GetNextToken();
+		Token* left = this->currentToken;
+		this->Eat(INTEGER);
+
+		Token* op = this->currentToken;
+		if (op->type == PLUS)
+		{
+			this->Eat(PLUS);
+		}
+		else
+		{
+			this->Eat(MINUS);
+		}
+
+		Token* right = this->currentToken;
+		this->Eat(INTEGER);
+		unsigned int result;
+
+		if (op->type == PLUS)
+		{
+			result = std::stoi(left->value) + std::stoi(right->value);
+		}
+		else
+		{
+			result = std::stoi(left->value) - std::stoi(right->value);
+		}
+
+		return result;
+	}
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::string input;
-	std::vector<Variable*> variables;
 
 	while (std::getline(std::cin, input))
 	{
-		if (input == "")
+		if (input != "")
 		{
-			return 0;
-		}
-		int equalIndex = input.find_first_of('=');
-		std::string name = input.substr(0, equalIndex);
-		int value = std::stoi(input.substr(equalIndex + 1, input.size()), &sz);
-		Variable *temp = new Variable(name, value);
-		variables.push_back(temp);
+			Interpreter* interpreter = new Interpreter(input);
+			unsigned long int result = interpreter->Expression();
 
-		std::cout << "name: " << temp->name << std::endl;
-		std::cout << "value: " << temp->value << std::endl;
+			std::cout << result << std::endl;
+		}
+
 	}
 	return 0;
 }
